0028706a65b06d3345c66c9770709c70bcaa24a7
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-28 19:08:48 +0100

    chore/setup

--- android/app/src/main/java/com/potb/MainActivity.java
+++ android/app/src/main/java/com/potb/MainActivity.java
@@ -1,6 +1,9 @@
 package com.potb;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "potb";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
