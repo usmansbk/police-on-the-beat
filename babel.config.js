4a63d83b6f2902065895fa99b2864847487a9630
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-29 17:37:00 +0100

    chore/setup mobx

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
     },
   },
-  plugins: [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }]
-  ]
 };
