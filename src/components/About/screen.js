c6dd3bfe7cd54a2fbf7658f8609ad1dd6e6abf5f
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-29 12:48:19 +0100

    chore

--- src/components/About/screen.js
+++ src/components/About/screen.js
@@ -19,24 +19,12 @@ export default () => (
 
     <Title>Legal</Title>
     <Paragraph>
-      Copyright 2020 Final year project 
+      Copyright 2020{'\n\n'}
+      Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.{'\n\n'}You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.{'\n\n'}See the License for the specific language governing permissions and limitations under the License.
     </Paragraph>
-    <Paragraph>
-      Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-    </Paragraph>
-    <Paragraph>
-      Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-    </Paragraph>
-    <Title>
-      Developers
-    </Title>
-    <Caption>Babakolo Usman Suleiman</Caption>
-    <Caption>Abubakar</Caption>
 
     <Title>Version</Title>
     <Caption>0.0.1-alpha</Caption>
-
-    <Title>Contact</Title>
-    <Caption>usmansbk@gmail.com</Caption>
   </View>
 );
