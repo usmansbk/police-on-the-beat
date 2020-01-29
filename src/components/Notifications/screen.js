4fd9ee1057b83bbd87af3fc32c55f6dad8e21e86
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-29 13:49:39 +0100

    chore

--- src/components/Notifications/screen.js
+++ src/components/Notifications/screen.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import { FlatList } from 'react-navigation';
+import { Headline } from 'react-native-paper';
 
-export default () => (
-  <View>
+export default class Notifications extends React.Component {
+  _renderEmpty = () => (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Image
+        source={require('../../assets/empty.png')}
+        style={{
+          width: 200,
+          height: 200
+        }}
+      />
+      <Headline style={{color: 'gray'}}>No Notifications</Headline>
+    </View>
+  );
 
-  </View>
-)
+  render() {
+    return (
+      <FlatList
+        data={[]}
+        contentContainerStyle={{
+          flex: 1,
+          backgroundColor: 'white'
+        }}
+        renderItem={this._renderItem}
+        ListEmptyComponent={this._renderEmpty}
+      />
+    );
+  }
+}
