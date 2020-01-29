fa29dcb1d41fe5a12a83e8fbf519e6ca62a8f2e8
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-29 18:15:53 +0100

    refactor/temp data

--- src/stores/data.js
+++ src/stores/data.js
@@ -1,6 +1,13 @@
 import { observable, action } from 'mobx';
 
 export default class Data {
-  @observable markers = [];
+  @observable markers = [{
+    title: 'Minor Theft',
+    description: 'Lorem',
+    location: {
+     latitude: 10.5823395,
+     longitude: 7.4202823
+    }
+  }];
 
 }
